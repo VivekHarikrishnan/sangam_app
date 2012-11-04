7ec4f43801b32bdac84a050a660d5ea6a0830111
Vivek <vivek@ubuntu.(none)>
2012-11-04 08:30:01 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SangamApp::Application.config.secret_token = '274ac73361626c72001c5bbcaf6dbc892a4f94ea1ca6ee95aad22adaffbfc20ea2e9c2242907e1cca6112de4d16da0781acafdd02226df734385df32e2443b29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SangamApp::Application.config.session_store :cookie_store, key: '_sangam_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SangamApp::Application.config.session_store :active_record_store
